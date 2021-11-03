db4082b225dcc6fd2005e0859f1577d97d62297c
usdruscia <40900784+usdruscia@users.noreply.github.com>
2021-11-03 12:55:34 -0400

    Fixed the ArgumentNullException caused by the UserSecrets configuration provider following the Azure Functions v2 to v3 migration.

--- src/Distech.CloudRelay.Functions/Startup.cs
+++ src/Distech.CloudRelay.Functions/Startup.cs
@@ -14,12 +14,16 @@ namespace Distech.CloudRelay.Functions
     class Startup
         : FunctionsStartup
     {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            // add user secrets configuration provider
+            builder.ConfigurationBuilder.AddUserSecrets<Startup>();
+
+            base.ConfigureAppConfiguration(builder);
+        }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            // register additional configuration providers
-            // official support is under investigation: https://github.com/Azure/azure-functions-host/issues/5274
-            AddConfigurations(builder);
-            
             // inject common services
             builder.Services.AddCloudRelayFileService((services, options) =>
             {
@@ -34,34 +38,6 @@ namespace Distech.CloudRelay.Functions
                         options.MinutesExpirationDelay = delay;
                     }
                 });
-        }
-
-        private void AddConfigurations(IFunctionsHostBuilder builder)
-        {
-            // keep track of all configuration providers prior to remove the existing IConfiguration service
-            var providers = new List<IConfigurationProvider>();
-
-            foreach (var descriptor in builder.Services.Where(descriptor => descriptor.ServiceType == typeof(IConfiguration)).ToList())
-            {
-                var existingConfiguration = descriptor.ImplementationInstance as IConfigurationRoot;
-
-                if (existingConfiguration is null)
-                    continue;
-
-                providers.AddRange(existingConfiguration.Providers);
-
-                builder.Services.Remove(descriptor);
-            }
-
-            // add custom configurations to builder
-            var configBuilder = new ConfigurationBuilder();
-            configBuilder.AddUserSecrets<Startup>();
-
-            // build custom configurations and add related providers to existing collection
-            providers.AddRange(configBuilder.Build().Providers);
-
-            // register the IConfiguration service using the updated list of providers
-            builder.Services.AddSingleton<IConfiguration>(new ConfigurationRoot(providers));
         }
     }
 }
