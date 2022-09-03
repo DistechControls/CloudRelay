5e0b1052064b0837226e4ec08da1696d1c295eba
ggirard07 <ggirard07@users.noreply.github.com>
2022-09-03 13:23:53 -0400

    WIP - Remove deprecated authentication dependencies

--- src/Distech.CloudRelay.API/Program.cs
+++ src/Distech.CloudRelay.API/Program.cs
@@ -10,8 +10,6 @@ using Distech.CloudRelay.API.Model;
 using Distech.CloudRelay.API.Options;
 using Distech.CloudRelay.API.Services;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -22,22 +20,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //inject authentication related entities and services
-#pragma warning disable 0618 //requires breaking changes to replace with Microsoft.Identity.Web
-builder.Services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
-#pragma warning restore 0618
-    .AddAzureADBearer(options => builder.Configuration.Bind(ApiEnvironment.AzureADOptions, options));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection(ApiEnvironment.AzureADOptions));
 
 if (builder.Environment.IsDevelopment())
 {
     //add diagnostics support for the JwtBearer middleware
-#pragma warning disable 0618 //requires breaking changes to replace with Microsoft.Identity.Web
-    builder.Services.PostConfigure<JwtBearerOptions>(AzureADDefaults.JwtBearerAuthenticationScheme, static options =>
-#pragma warning restore 0618
+    builder.Services.PostConfigure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, static options =>
     {
         options.Events = JwtBearerMiddlewareDiagnostics.Subscribe(options.Events);
     });
